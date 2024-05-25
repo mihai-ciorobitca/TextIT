ded99199228a1efda470a8766a7b8aca67699f72
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-25 12:20:47 +0300

    Upload

--- api/app.py
+++ api/app.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask, render_template, redirect, session, request
 from supabase import create_client
-from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
-import os
+# from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "1234567890"
 app.debug = True
-socketio = SocketIO(app)
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
@@ -28,19 +26,20 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        session['email'] = email
-        return redirect("/")
+        user = supabase.from_('users').select('*').eq('email', email).eq('password', password).execute()
+        if user:
+            session['email'] = email
+            return redirect("/")
     return render_template('login.html')
 
-@socketio.on("send_message")
-def handle_message(data):
-    text = data["text"]
-    sender = session["email"]
-    supabase.table("messages").insert({"sender": sender, "text": text}).execute()
-    emit("new_message", {"sender": sender, "text": text}, broadcast=True)
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    sender = session.get('email')
+    text = data.get('text')
 
-def handle_change(event, sid):
-    socketio.emit('new_message', event.data, room=sid)
+    supabase.table('messages').insert({'sender': sender, 'text': text}).execute()
+    return {'success': True}
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
