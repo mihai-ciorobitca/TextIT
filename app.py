c3f95b75298a651a0fd77c5ddec2bed949e43db8
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-25 08:35:06 +0300

    Upload

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.debug = True
 socketio = SocketIO(app)
 
-MONGO_URI = "mongodb://localhost:27017/"
-#"mongodb+srv://mihaiciorobitca:R3dwaLL2013star@cluster.rsenaqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
+MONGO_URI = "mongodb+srv://mihaiciorobitca:R3dwaLL2013star@cluster.rsenaqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
 mongo = MongoClient(MONGO_URI)
 
 @app.route('/')
@@ -39,4 +38,5 @@ def handle_message(data):
     mongo.db.messages.insert_one({"sender": sender, "text": text})
     emit("new_message", {"sender": sender, "text": text}, broadcast=True)
 
-app.run()
+if __name__ == "__main__":
+    app.run()
