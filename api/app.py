807fe454170af2afe8103cb050aa9a4ddd5420e0
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-25 20:39:25 +0300

    Upload

--- api/app.py
+++ api/app.py
@@ -9,14 +9,18 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = "1234567890"
 
-url = environ.get("SUPABASE_URL")
-key = environ.get("SUPABASE_SECRET")
-supabase = create_client(url, key)
+supabase_url = environ.get("SUPABASE_URL")
+supabase_anon_key = environ.get("SUPABASE_SECRET")
+supabase = create_client(supabase_url, supabase_anon_key)
 
 @app.route('/')
 def index():
     if session.get('email', False):
-        return render_template('index.html', email=session["email"])
+        return render_template(
+            'index.html', email=session["email"],
+            supabase_url=supabase_url,
+            supabase_anon_key=supabase_anon_key
+        )
     return redirect("/login")
 
 @app.route('/login', methods=['GET', 'POST'])
--- api/test.py
+++ api/test.py
@@ -8,18 +8,10 @@ load_dotenv()
 app = Flask(__name__)
 
 supabase_url = os.environ.get("SUPABASE_URL")
-supabase_key = os.environ.get("SUPABASE_KEY")
+supabase_key = os.environ.get("SUPABASE_SECRET")
 supabase: Client = create_client(supabase_url, supabase_key)
+email = "mihai@gmail.com"
 
-@app.route("/")
-def index():
-    return render_template("test.html", supabase_url=supabase_url, supabase_key=supabase_key)
+user = supabase.table('users').select('*').eq('email', email).execute()
 
-@app.route("/send_message", methods=["POST"])
-def send_message():
-    message = request.form.get("message")
-    supabase.table("messages").insert({"content": message}).execute()
-    return "OK"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print(user)
